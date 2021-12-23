ae842be073bdd41bbd492676388cdf8725c44e87
Roopa Jamadar <roopajamadar14@gmail.com>
2021-12-23 23:23:29 +0530

    [Roopa] Added:Analyse and Respond Happy Or Sad Mood

---
+++ MoodAnalyserDemo/MoodAnalyserDemo/MoodAnalyser.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MoodAnalyserDemo
+{
+    public class MoodAnalyser
+    {
+        public string msg;
+
+        public string AnalyseMood(string msg)
+        {
+            if (msg.ToLower().Contains("sad"))
+                return "SAD";
+
+            else
+                return "HAPPY";
+        }
+
+    }
+}
---
+++ MoodAnalyserDemo/MoodAnalyserTest/UnitTest1.cs
@@ -0,0 +1,35 @@
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using MoodAnalyserDemo;
+
+namespace MoodAnalyserTest
+{
+    [TestClass]
+    public class UnitTest1
+    {
+        [TestMethod]
+        public void msgSadMood()
+        {
+            string msg = "I am in sad mood";
+            string expected = "SAD";
+
+            MoodAnalyser mood = new MoodAnalyser();
+
+            string actual = mood.AnalyseMood(msg);
+
+            Assert.AreEqual(expected, actual);
+        }
+        [TestMethod]
+        public void msgHaapyMood()
+        {
+            string msg = "I am in Any mood";
+            string expected = "HAPPY";
+
+            MoodAnalyser mood = new MoodAnalyser();
+
+            string actual = mood.AnalyseMood(msg);
+
+            Assert.AreEqual(expected, actual);
+        }
+
+    }
+}
